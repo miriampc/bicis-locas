c93bbc5a7d9cf116280ff747fc21e8620d0b07c7
Miriam Peralta <miiriampc@gmail.com>
2017-03-02 18:36:22 -0500

    agregando funciones en bicis-locas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
-function validateForm(){
+document.getElementById('registra').onclick= function validateForm(){
 	/* Escribe tú código aquí */
+  var name=document.getElementById('name').value;
+  console.log()
+  var lastname=document.getElementById('lastname').value;
+  var email=document.getElementById('input-email').value;
+  var password=document.getElementById('input-password').value;
+
+  function noVacios(valor){
+    if(valor==null || valor.length==0 ){//|| /\s/.test(valor)){
+      alert("El campo "+valor +" esta vacio debe llenar es obligatorio")
+    }
+  }
+  function soloLetras(valor){
+    if(!(/[A-Za-z]/.test(valor)){
+      alert("El campo "+valor +" esta vacio debe llenar es obligatorio")
+    }
+
+  }
+
+  noVacios(name);
+  noVacios(lastname);
+  noVacios(email);
+  noVacios(password);
+
+  soloLetras(name);
+  soloLetras(lastname);
+
 }
+
+/*document.getElementById('form').onsubmit=function (e){
+  e.preventDefault();
+}*
