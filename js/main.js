1784469fc3dd3e6035a485b455dbc9f9183ad359
miriampc <miiriampc@gmail.com>
2017-03-02 23:41:55 -0500

    validacion de letras y email

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
 document.getElementById('registra').onclick= function validateForm(){
-	/* Escribe tú código aquí */
+	/* Escribe tú código aquí*/
   var name=document.getElementById('name').value;
-  console.log()
+  //console.log()
   var lastname=document.getElementById('lastname').value;
   var email=document.getElementById('input-email').value;
   var password=document.getElementById('input-password').value;
 
   function noVacios(valor){
     if(valor==null || valor.length==0 ){//|| /\s/.test(valor)){
-      alert("El campo "+valor +" esta vacio debe llenar es obligatorio")
+      alert("El campo "+valor +" esta vacio debe llenar es obligatorio");
     }
   }
   function soloLetras(valor){
-    if(!(/[A-Za-z]/.test(valor)){
-      alert("El campo "+valor +" esta vacio debe llenar es obligatorio")
+    if(!(/[A-ZÑÁ-Úa-zñá-ú\s]{1,}/.test(valor))){
+      alert("El campo "+valor +"solo acepta letras")
     }
-
   }
-
+  function validaEmail(!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(valor))){
+    return false;
+  }
   noVacios(name);
   noVacios(lastname);
   noVacios(email);
@@ -26,8 +27,9 @@ document.getElementById('registra').onclick= function validateForm(){
   soloLetras(name);
   soloLetras(lastname);
 
-}
+  validaEmail(email);
 
+}
 /*document.getElementById('form').onsubmit=function (e){
   e.preventDefault();
-}*
+}*/
