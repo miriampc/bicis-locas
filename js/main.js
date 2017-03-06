b6185ae3c70db66303ddd93660161d13b88dffd8
miriampc <miiriampc@gmail.com>
2017-03-05 20:06:29 -0500

    refactorizando codigo

--- js/main.js
+++ js/main.js
@@ -5,67 +5,50 @@ function validateForm(){
   var password=document.getElementById('input-password').value;
   var tipobici=document.getElementById('tipobici').value;
 
-  function noVacios(valor){
+//Funciones de Validación
+  function validaNombreApellido(valor){
     if(valor.length==0 || valor==null){
-      alert("Este campo esta vacio, debe llenar es obligatorio");
+      alert("Este campo es obligatorio");
       return false;
-    }
-    return true;
-  }
-
-  function soloLetras(valor){
-    if(!(/[A-ZÑÁ-Úa-zñá-ú\s]{1,}/.test(valor))){
+    }else if(!(/^([A-Za-zÑñá-úÁ-Ú ]+)$/.test(valor))){
       alert("Este campo solo acepta letras");
       return false;
-    } return true;
-  }
-
-  function validaMayus(valor){
-    if(!(/[^a-z-0-9\s]+([a-z]{2,})/.test(valor))){
+    }else if(!(/[^a-zá-ú-0-9\s]+([a-zá-ú ]{2,})/.test(valor))){
       alert("La primera letra debe ser mayuscula");
       return false;
     }
     return true;
   }
+
   function validaEmail(valor){
-      if (!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(valor))){
-          return false;
-      }
-      return true;
-}
-function pwd(valor){
-  if(valor.length<6){
-    alert("El password debe tener almenos seis caracteres");
-  }else if (valor=="098754" || valor=="123456" || valor=="password") {
-    alert("El password debe ser diferente de 123456, 098754 y password");
-    return false;
-  }else{ return true;}
-}
-
-
-  if(noVacios(name)){
-      if(soloLetras(name)){
-        if(validaMayus(name)){
-          if(noVacios(lastname)){
-              if(soloLetras(lastname)){
-                if(validaMayus(lastname)){
-                    if(noVacios(email)){
-                        validaEmail(email);
-                        if(noVacios(password)){
-                          if(pwd(password)){
-                            if(tipobici==0){
-                                alert("Debe seleccionar una opcion");
-                                return false;
-                            }else {
-                              return alert("Gracias todos los datos fueron enviados!");
-                            }
-                          }
-                        }
-                    }
-                }
-              }
-            }
-        }
+    if(valor.length==0 || valor==null){
+      alert("Debe ingresar su correo electrónico es obligatorio");
+      return false;
     }
-  }  return false;
+    validaPassword(password);
+  }
+  function validaPassword(valor){
+    if(valor.length<6){
+      alert("Debe ingresar su password, debe tener almenos seis caracteres");
+      return false;
+    }else if (valor=="098754" || valor=="123456" || valor=="password") {
+      alert("El password debe ser diferente de 123456, 098754 y password");
+      return false;
+    }
+    validaSeleccion(tipobici);
+  }
+  function validaSeleccion(valor){
+    if(valor==0){
+      alert("Debe seleccionar una opcion");
+      return false;
+    }
+    alert("Gracias todos los datos fueron enviados!");
+    document.getElementById('name').value="";
+    document.getElementById('lastname').value="";
+    document.getElementById('input-email').value="";
+    document.getElementById('input-password').value="";
+    document.getElementById('tipobici').value=0;
+  }
+  if(validaNombreApellido(name)){if(validaNombreApellido(lastname)){validaEmail(email)}return false;}
+
 }
